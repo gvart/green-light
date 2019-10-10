f9ce2a8b82fd04b329e7d14f4bad4d0482e1e5cc
Vladlen Gladis <vladlen.gladis@extendaretail.com>
2019-10-10 18:36:59 +0300

    Configure jacoco merging configure circleci

---
+++ config-server/src/test/java/com/greenlight/configserver/ConfigServerTests.java
@@ -0,0 +1,11 @@
+package com.greenlight.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigServerLauncher.class)
+public class ConfigServerTests {
+
+  @Test
+  public void contextLoads() {}
+}
---
+++ registry-service/src/test/java/com/greenlight/registry/RegistryServiceTests.java
@@ -0,0 +1,11 @@
+package com.greenlight.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RegistryServiceLauncher.class)
+public class RegistryServiceTests {
+
+  @Test
+  public void contextLoads() {}
+}
