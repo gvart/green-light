86a9e2aed94746ac75684bc011b70477441b0362
vgladis <vladlen.gladis@extendaretail.com>
2020-02-24 21:05:03 +0200

    * Add pub key for jwt token * include auth-service gradle.kts

---
+++ auth-service/build.gradle.kts
@@ -0,0 +1,27 @@
+import com.google.cloud.tools.jib.gradle.ContainerParameters
+
+version = "0.0.1-SNAPSHOT"
+
+val containerProperties = ContainerParameters().apply {
+    jvmFlags = listOf()
+}
+dependencies {
+    compile(project(":common"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-oauth2")
+    implementation("org.springframework.security:spring-security-jwt")
+
+    runtime("org.postgresql:postgresql")
+    implementation("org.liquibase:liquibase-core")
+    //kotlin specific
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+
+}
